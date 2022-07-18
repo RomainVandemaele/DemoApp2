655a2ae57cc45c95f745190197296a2939782ee6
rvdemael <romain.v@live.be>
2022-07-18 08:31:20 +0200

    config

---
+++ app/src/main/java/be/bf/android/demoapp/configs/Config.java
@@ -0,0 +1,10 @@
+package be.bf.android.demoapp.configs;
+
+public abstract class Config {
+
+    public static class Preferences {
+        public static final String EMAIL_PREF_KEY = "email";
+        public static final String PASSWORD_PREF_KEY = "password";
+        public static final String CHECKED_PREF_KEY = "rememberMe";
+    }
+}
